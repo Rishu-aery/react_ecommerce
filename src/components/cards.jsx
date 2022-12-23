4ad828c4c568440ecedba05af79aedf98e5f99bd
unknown <aeryrishab0@gmail.com>
2022-12-23 17:25:41 +0530

    Static cards done

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -4,15 +4,86 @@ import "../css/cards.css"
 export default function cards() {
   return (
     <div className='container'>
-        <img className='image' src="https://i.ytimg.com/vi/MyDnyfS7c1g/maxresdefault.jpg"/>
-        <div>
-            <div className='card'>
-                <img src="https://fakeimg.pl/1000x50" alt="Product Image" />
-                <h2>Drift 3M 3x3 (Magnetic)</h2>
-                <p>₹ 599</p>
-                <p>• in stock</p>
-            </div>
+      <img className='image' src="https://i.ytimg.com/vi/MyDnyfS7c1g/maxresdefault.jpg" />
+      <div className='all_cards'>
+        <div className='card'>
+          <img src="https://fakeimg.pl/1000x50" alt="Product Image" />
+          <h2>Drift 3M 3x3 (Magnetic)</h2>
+          <p>₹ 599</p>
+          <div className='star'>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+          <p>• in stock</p>
         </div>
+
+        <div className='card'>
+          <img src="https://fakeimg.pl/1000x50" alt="Product Image" />
+          <h2>Drift 3M 3x3 (Magnetic)</h2>
+          <p>₹ 599</p>
+          <div className='star'>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+          <p>• in stock</p>
+        </div>
+
+        <div className='card'>
+          <img src="https://fakeimg.pl/1000x50" alt="Product Image" />
+          <h2>Drift 3M 3x3 (Magnetic)</h2>
+          <p>₹ 599</p>
+          <div className='star'>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+          <p>• in stock</p>
+        </div>
+
+        <div className='card'>
+          <img src="https://fakeimg.pl/1000x50" alt="Product Image" />
+          <h2>Drift 3M 3x3 (Magnetic)</h2>
+          <p>₹ 599</p>
+          <div className='star'>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+          <p>• in stock</p>
+        </div>
+
+        <div className='card'>
+          <img src="https://fakeimg.pl/1000x50" alt="Product Image" />
+          <h2>Drift 3M 3x3 (Magnetic)</h2>
+          <p>₹ 599</p>
+          <div className='star'>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+          <p>• in stock</p>
+        </div>
+
+        <div className='card'>
+          <img src="https://fakeimg.pl/1000x50" alt="Product Image" />
+          <h2>Drift 3M 3x3 (Magnetic)</h2>
+          <p className='price'>₹ 599</p>
+          <div className='star'>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+          </div>
+          <p className='stock'>• in stock</p>
+        </div>
+      </div>
     </div>
   )
 }
